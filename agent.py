71e5298a32d38b0189e42663ac27b9d6ce67eb75
zineblahrichi <zineb.lahrichi@student-cs.fr>
2022-03-03 16:53:47 +0100

    Added agent file

---
+++ agent.py
@@ -0,0 +1,16 @@
+class Network:
+    pass
+
+
+class Agent:
+    def __init__(self):
+        pass
+
+    def select_action(self, state):
+        pass
+
+    def policy(self):
+        pass
+
+    def update(self):
+        pass
