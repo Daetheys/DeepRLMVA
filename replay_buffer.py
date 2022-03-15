bb53fe159bad7157683149715791581d246b616c
Tim-blo <timothee.blondiaux@quincus.com>
2022-03-15 18:26:17 +0100

    Adding prioritized replay buffer.

--- replay_buffer.py
+++ replay_buffer.py
@@ -59,3 +59,50 @@ class ReplayBuffer(BaseReplayBuffer):
             stacked_transitions.append(arrays)
 
         return Transition(*stacked_transitions)
+
+
+class PrioritizedReplayBuffer:
+    """Fixed-size buffer to store transition tuples."""
+
+    def __init__(self, buffer_capacity: int):
+        """Initialize a ReplayBuffer object."""
+        self._memory = list()
+        self._priority = list()
+        self._maxlen = buffer_capacity
+
+    def add(self, obs_tm1, action_tm1, reward_t, discount_t, obs_t, done,
+            priority):
+        """Add a new transition to memory."""
+        if len(self._memory) >= self._maxlen:
+            self._memory.pop(0)  # remove first elem (oldest)
+            self._priority.pop(0)
+
+        transition = Transition(
+            obs_tm1=obs_tm1,
+            action_tm1=action_tm1,
+            reward_t=reward_t,
+            discount_t=discount_t,
+            obs_t=obs_t,
+            done=done)
+
+        # convert every data into jnp array
+        transition = jax.tree_map(jnp.array, transition)
+
+        self._memory.append(transition)
+        self._priority.append(priority)
+
+    def sample(self):
+        """Randomly sample a transition from memory."""
+        assert self._memory, 'replay buffer is unfilled'
+        assert len(self._memory) == len(self._priority)
+
+        priority_sum = sum(self._priority)
+        normalized_priority = [p / priority_sum for p in self._priority]
+
+        transition_idx = np.random.choice(range(len(self._memory)),
+                                          p=normalized_priority)
+
+        transition = self._memory.pop(transition_idx)
+        self._priority.pop(transition_idx)
+
+        return transition
