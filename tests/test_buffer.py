f99386e19363ff8a4fcb9fa8e5e24a9a59a82208
Tim-blo <timothee.blondiaux@quincus.com>
2022-03-15 18:34:53 +0100

    Adding tests with the prioritized replay buffer.

--- tests/test_buffer.py
+++ tests/test_buffer.py
@@ -34,3 +34,25 @@ def test_sample_batch():
     for i in range(5):
         _ = buffer.sample_batch(10)
     assert not buffer._memory
+
+
+def test_add_to_prioritized_replay_buffer():
+    buffer = PrioritizedReplayBuffer(20)
+    assert not buffer._memory
+    assert not buffer._priority
+    buffer.add(0, 0, 0, 0, 0, True, 0.1)
+    assert buffer._memory
+    assert buffer._priority
+    for i in range(50):
+        buffer.add(i, i, i, i, i, True, i)
+    assert len(buffer._memory) == 20
+    assert len(buffer._priority) == 20
+
+
+def test_sample_from_prioritized_replay_buffer():
+    buffer = PrioritizedReplayBuffer(20)
+    for i in range(50):
+        buffer.add(i, i, i, i, i, True, 0)
+    buffer.add(50, 50, 50, 50, 50, True, 50)
+    sample = buffer.sample()
+    assert sample[4] == 50
