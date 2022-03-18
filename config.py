00700b506408962100949578172005add71a0a64
zineblahrichi <zineb.lahrichi@student-cs.fr>
2022-03-18 17:41:19 +0100

    Move configurations to config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+DEFAULT_AGENT_CONFIG = {
+    "clip_eps": 0.1
+}
+
+DEFAULT_TRAINER_CONFIG = {
+  "nb_fit_per_epoch":5,
+  "train_batch_size":512,
+  "training_rollout_length":1024,
+  "learning_rate":0.01
+}
+
+DEFAULT_REPLAY_BUFFER_CONFIG = {}
+
+DEFAULT_ROLLOUT_CONFIG = {}
+
