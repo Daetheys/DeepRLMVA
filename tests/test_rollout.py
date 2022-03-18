6162a520eaedfe689797b2fec4839fea83288bb0
EVA <eva.zagury@dauphine.eu>
2022-03-18 18:44:25 +0100

    debug rollout

--- tests/test_rollout.py
+++ tests/test_rollout.py
@@ -1,14 +1,22 @@
 import numpy as np
+import jax
+import jax.numpy as jnp
 from statistics import mean
 
 from rollout import *
+from replay_buffer import BaseReplayBuffer
+from networks import actor_critic_net
+import agent 
 
 def test_rollout():
-    agent = 0
+    select_action = agent.select_action_discrete
     env = 0
-    nb_steps = 0
-    replay_buffer = 0
+    nb_steps = 50
+    replay_buffer = BaseReplayBuffer(5)
     discount = 0.99
-    params, apply = 0,0
+    params= init(rng, jnp.zeros(10))
+    net = actor_critic_net(5)
+    init, apply = net
     rng = jax.random.PRNGKey(42)
-    infos, mean_reward, mean_timestep = rollout(agent, env, nb_steps, replay_buffer, discount, params, apply, rng)
+    infos, mean_reward, mean_timestep = rollout(select_action, env, nb_steps, replay_buffer, discount, params, apply, rng)
+    assert infos.shape == (4, 50)
