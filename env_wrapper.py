d7095b45e87a04ada9b8ea58f495273dc8492443
Nicolas Yax <daetheys@gmail.com>
2022-03-18 18:32:06 +0100

    Added env_wrapper for jax

--- env_wrapper.py
+++ env_wrapper.py
@@ -1,2 +1,8 @@
+import numpy as np
+import gym
 class EnvWrapper(gym.Wrapper):
-    pass
+    def __init__(self,env):
+        super().__init__(env)
+        self.env = env
+    def step(self,a):
+        return self.env.step(np.array(a))
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from trainer import Trainer,DEFAULT_CONFIG
 import gym
 from networks import actor_critic_net
+from env_wrapper import EnvWrapper
 
-env_creator = lambda : gym.make('CartPole-v1')
+env_creator = lambda :EnvWrapper(gym.make('CartPole-v1'))
 env = env_creator()
 net = actor_critic_net(env.action_space.n)
 
