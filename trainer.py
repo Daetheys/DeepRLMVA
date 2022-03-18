d0aeb17702e73353965dd9dc3056f4c31123144d
Nicolas Yax <daetheys@gmail.com>
2022-03-18 18:00:27 +0100

    Fixed parameters position in trainer

--- trainer.py
+++ trainer.py
@@ -9,7 +9,8 @@ DEFAULT_CONFIG = {
   "train_batch_size":512,
   "training_rollout_length":1024,
   "learning_rate":0.01,
-  "clip_eps":0.1
+  "clip_eps":0.1,
+  "seed":42
 }
 
 jitted_rollout = jax.jit(rollout)
@@ -25,27 +26,26 @@ class Trainer:
 
     self.replay_buffer = ReplayBuffer(1e5)
 
-    self.config = config
+    self.reset()
 
   def reset(self):
     self.train_env = self.env_creator()
     self.eval_env = self.env_creator()
 
-    self.rng = jax.random.PRNGKey(42)
+    self.rng = jax.random.PRNGKey(self.config['seed'])
     self.params = self.net.init(x=self.train_env.observation_space.sample(),rng=self.rng)
     self.opt = optax.adam(self.config['learning_rate'])
     self.opt_state = self.opt.init(self.params)
-    self.clip_eps = 0.1
 
   def train(self,nb_steps):
     for i in range(nb_steps):
       #Training Rollout
-      jitted_rollout(self.train_env,self.agent,self.config['training_rollout_length'],replay_buffer=self.replay_buffer,discount=self.config['gamma'])
+      jitted_rollout(self.train_env,self.config['training_rollout_length'],self.replay_buffer,discount=self.config['gamma']) #rng,params,apply
       for j in range(self.config['nb_fit_per_epoch']):
         batch = self.replay_buffer.sample_batch(self.config['train_batch_size'])
         new_params = self.params
-        new_params,new_opt_state = jitted_update(self.params,batch,self.opt,self.opt_state,self.clip_eps,self.params)
+        new_params,new_opt_state = jitted_update(self.params,batch,self.opt,self.opt_state,self.config['clip_eps'],self.params)
       self.params = new_params
       #Eval Rollout
-      stats = jitted_rollout(self.eval_env,self.agent,self.config['testing_rollout_length'],discount=self.config['gamma'])
+      stats = jitted_rollout(self.eval_env,self.config['testing_rollout_length'],discount=self.config['gamma'])
       print('stats',stats) #Print stats about what's happening during training
