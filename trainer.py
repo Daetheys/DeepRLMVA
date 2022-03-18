516a26071ac2fc657d429831918f6c7389a97cef
Nicolas Yax <daetheys@gmail.com>
2022-03-18 17:33:00 +0100

    Updated trainer with new signature

--- trainer.py
+++ trainer.py
@@ -8,7 +8,8 @@ DEFAULT_CONFIG = {
   "nb_fit_per_epoch":5,
   "train_batch_size":512,
   "training_rollout_length":1024,
-  "learning_rate":0.01
+  "learning_rate":0.01,
+  "clip_eps":0.1
 }
 
 jitted_rollout = jax.jit(rollout)
@@ -22,7 +23,7 @@ class Trainer:
     self.config = config
     
 
-    self.replay_buffer = ReplayBuffer()
+    self.replay_buffer = ReplayBuffer(1e5)
 
     self.config = config
 
@@ -39,12 +40,12 @@ class Trainer:
   def train(self,nb_steps):
     for i in range(nb_steps):
       #Training Rollout
-      jitted_rollout(self.train_env,self.agent,self.config['training_rollout_length'],replay_buffer=self.replay_buffer)
+      jitted_rollout(self.train_env,self.agent,self.config['training_rollout_length'],replay_buffer=self.replay_buffer,discount=self.config['gamma'])
       for j in range(self.config['nb_fit_per_epoch']):
-        batch = self.replay_buffer.sample(self.config['train_batch_size'])
+        batch = self.replay_buffer.sample_batch(self.config['train_batch_size'])
         new_params = self.params
         new_params,new_opt_state = jitted_update(self.params,batch,self.opt,self.opt_state,self.clip_eps,self.params)
       self.params = new_params
       #Eval Rollout
-      stats = jitted_rollout(self.eval_env,self.agent,self.config['testing_rollout_length'])
+      stats = jitted_rollout(self.eval_env,self.agent,self.config['testing_rollout_length'],discount=self.config['gamma'])
       print('stats',stats) #Print stats about what's happening during training
