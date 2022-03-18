88e8b346a6d23c9b8896ead20dc32e5bacc25f69
EVA <eva.zagury@dauphine.eu>
2022-03-18 18:20:40 +0100

    changes in rollout

--- tests/test_rollout.py
+++ tests/test_rollout.py
@@ -4,4 +4,11 @@ from statistics import mean
 from rollout import *
 
 def test_rollout():
-   pass
+    agent = 0
+    env = 0
+    nb_steps = 0
+    replay_buffer = 0
+    discount = 0.99
+    params, apply = 0,0
+    rng = jax.random.PRNGKey(42)
+    infos, mean_reward, mean_timestep = rollout(agent, env, nb_steps, replay_buffer, discount, params, apply, rng)
